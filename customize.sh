06d51af378f8f9008a0e309f41dd823a811b735b
weiwuji1 <weiwuji1@163.com>
2026-04-20 11:45:05 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,16 +8,16 @@ set -e
 
 echo "=== 开始定制配置 ==="
 
-# [1/3] 修改默认 IP 地址为 192.168.100.1 (适配 ImmortalWrt 结构)
+# [1/3] 修改默认 IP 地址为 10.1.1.110 (适配 ImmortalWrt 结构)
 echo "[1/3] 修改默认 IP 地址..."
 NETWORK_FILE="package/base-files/files/etc/config/network"
 if [ -f "$NETWORK_FILE" ]; then
     # 备份原文件
     cp $NETWORK_FILE $NETWORK_FILE.bak
     # 修改 lan 接口 IP
-    sed -i 's/ipaddr=192\.168\.1\.1/ipaddr=192.168.100.1/g' $NETWORK_FILE
-    sed -i 's/ipaddr=\"192\.168\.1\.1\"/ipaddr=\"192.168.100.1\"/g' $NETWORK_FILE
-    echo "默认 IP 已修改为 192.168.100.1"
+    sed -i 's/ipaddr=192\.168\.1\.1/ipaddr=10.1.1.110/g' $NETWORK_FILE
+    sed -i 's/ipaddr=\"192\.168\.1\.1\"/ipaddr=\"10.1.1.110\"/g' $NETWORK_FILE
+    echo "默认 IP 已修改为 10.1.1.110"
 else
     echo "警告: 未找到 network 配置文件，跳过 IP 修改。"
 fi
